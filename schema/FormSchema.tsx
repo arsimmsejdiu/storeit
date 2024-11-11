8c64543674e3aafcf7456e50cc12fa78636b0d57
arsimsejdiu <arsim.sejdiu@bik-air.com>
2024-11-11 21:40:01 +0100

    Stoorage manager

---
+++ schema/FormSchema.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { FormType } from "@/models/FormType";
+import { z } from "zod";
+
+export const authFormSchema = (formType: FormType) => {
+  return z.object({
+    email: z.string().email(),
+    fullName:
+      formType === "sign-up"
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+  });
+};
